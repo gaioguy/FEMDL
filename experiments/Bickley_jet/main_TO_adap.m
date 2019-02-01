75bd794e22034bb248a826cda385bab52cf3a020
gaioguy <oj@tum.de>
2019-02-01 17:16:04 +0100

    fixed cylindrical Delaunay triangulation bug

--- experiments/Bickley_jet/main_TO_adap.m
+++ experiments/Bickley_jet/main_TO_adap.m
@@ -8,33 +8,28 @@ T = @(x) flow_map(@bickleyjet,x,linspace(t0,tf,nt));
 dim = 2; % dimension of state space
 nx = 100;  ny = nx/20*6;  n = nx*ny;
 [xi,yi] = meshgrid(linspace(0,20.01,nx),linspace(-3,3,ny));
-p0 = [xi(:) yi(:)];  
+p0 = [xi(:) yi(:)]; 
+pb = [1:n; 1:n]';
 
 %% time integration
 tic; P = T(p0); toc
 for k = 1:nt, p{k} = [mod(P(:,k),20) P(:,k+nt)]; end;
 
 %% assembly
-tic; 
-pm = 1;                               % percentage of nodes to keep
-pm = 0.2;                               
+tic; pm = 0.2;                               
 D = sparse(n,n); M = sparse(n,n);
 for k = 1:nt
-    [p1,~,pb] = delaunay_C2(p{k},[0,20]);
-    n1 = size(p1,1);
-    % r1 = randperm(n1,floor(pm*n1))';
-    r1 = randsample(n1-n:n,floor(pm*n))';
-    tr = delaunay(p1(r1,:));
-    t = r1(tr);
+    r = randperm(n,floor(pm*n))';
+    tr = delaunay_C2(p{k}(r,:),[0,20]);
+    t = r(tr);
     I = kron([1 0 1],ones(size(t,1),1));
-    [Dt,Mt] = assemble(p1,t,pb,I);
-    D = D + Dt; M = M + Mt; 
-end; toc
+    [Dk, Mk] = assemble(p{k},t,pb,I);
+    D = D + Dk; M = M + Mk; 
+end; toc;
 
 %% remove all zero rows and columns
 S = sum(abs(D)); I = find(abs(S)>eps); 
 D = D(I,I); M = M(I,I); 
-[pI,tI,pbI] = delaunay_C2(p{1}(I,:),[0,20]);
 
 %% eigenproblem
 tic; [V,L] = eigs(D,M,15,'SM');  toc
@@ -45,16 +40,17 @@ figure(1); clf; plot(lam,'*'); axis tight, axis square
 xlabel('$k$'); ylabel('$\lambda_k$')
 
 %% plot eigenvector
-figure(2); ev_no = 4;
+figure(2); ev_no = 6;
+pI = p{1}(I,:); tI = delaunay(pI); pbI = [1:length(I);1:length(I)]';
 plotf(pI,tI,pbI,normed(V(:,ev_no)),0); caxis([-1,1]); colorbar
 xlabel('$x$'); ylabel('$y$'); 
 
 %% compute partition
 nx1 = 400; ny1 = nx1/20*6; x1 = linspace(0,20,nx1); y1 = linspace(-3,3,ny1);
 [X1,Y1] = meshgrid(x1,y1); 
-nc = 8; W(pbI(:,1),:) = V(pbI(:,2),:); 
-V1 = eval_p1(pI,W(:,1:nc),[X1(:) Y1(:)]);  % evaluate eigenvectors on grid
-idx = kmeans(V1,nc,'Replicates',10);       % kmeans clustering
+nc = 8;  
+V1 = eval_p1(pI,V(:,1:nc),[X1(:) Y1(:)]);  % evaluate eigenvectors on grid
+idx = kmeans(V1,nc,'Replicates',20);       % kmeans clustering
 
 %% plot partition
 figure(3); subplot(313);  clf; 
--- src/delaunay_C2.m
+++ src/delaunay_C2.m
@@ -1,58 +1,14 @@
-function [p1,t3,pb] = delaunay_C2(p, box)
-
-%% DELAUNAY_C2 construct Delaunay triangulation on cylinder
-%
-% [p1,t3,pb] = delaunay_C2(p, box) constructs a Delaunay
-% triangulation on the 2-dimensiona cylinder (i.e. periodic in x-direction)
-%   p: (n x 2), nodes of the triangulation, one node per row
-%   box: (1 x 2), specifies the domain in x-direction
-%   p1: (n1 x 2), extended set of nodes
-%   t3: (m3 x 3), triangulation, each row def. one triangle by indexing
-%   into p1
-%   pb: (n1 x 2), boundary map: node pb(i,1) is identified with pb(:,2)
-%
-% (C) 2017 by O. Junge, see COPYRIGHT 
+function t3 = delaunay_C2_new(p, box)
 
 n = size(p,1);
-xmin = box(1); xmax = box(2);
+dx = box(2)-box(1); 
+dp = [dx 0; -dx 0];             
+P = [p; p+dp(1,:); p+dp(2,:)];  % copy vertices to the left and the right
+p_map = [1:n, 1:n, 1:n];
 
-%% copy vertices around
-dx = xmax-xmin; 
-dp = [dx 0; -dx 0];
-P = [p; p+dp(1,:); p+dp(2,:)];
-%clf; scatter(P(:,1),P(:,2),10,'b','filled'); hold on
-PB = repmat(1:n,1,3);
-
-%% construct triangulation
-aS = alphaShape(P(:,1),P(:,2),1);
-t = alphaTriangulation(aS); 
-%triplot(t,P(:,1),P(:,2),'b'); hold on; axis tight
-
-%% extract triangles with at least one vertex in original domain
-in = 1:n;           % vertices in original domain
-%scatter(P(in,1),P(in,2),3,'r','filled'); 
-tr = triangulation(t,P);
-ti = vertexAttachments(tr,in');   % indices of triangles adjacent to each vertex in original domain
-adj = [];
-for i = 1:length(in), adj = [adj ti{i}]; end
-adj = unique(adj);
-t1 = t(adj,:);
-%triplot(t1,P(:,1),P(:,2),'g');
-
-%% remove duplicate triangles
-flags = sum(t1 > 2*n,2) == 0;  % test if some vertex of a triangle is outside
-t2 = t1(find(flags),:);
-%triplot(t2,P(:,1),P(:,2),'m');
-
-%% extract required points and corresponding triangles
-all = unique(t2(:)); 
-p1 = P(all,:);
-Kt(all) = 1:length(all); Kt = Kt(:);        % mapping from old vertex numbers to 1:no_of_vertices_in_p1
-t3 = [Kt(t2(:,1)) Kt(t2(:,2)) Kt(t2(:,3))];
-% triplot(t3,p1(:,1),p1(:,2),'k'); hold on
-
-%% compute boundary mapping
-out = setdiff(all,in)'; 
-in = setdiff(all,out)';
-pb = ([1:length(all); in PB(out)])';
-% scatter(P(out,1),P(out,2),10,'r','filled');  axis tight
+t = delaunay(P(:,1),P(:,2));    % construct triangulation
+I = find(sum(t > 2*n,2) == 0);  % find triangles with at least one vertex in left copy of domain
+t1 = t(I,:);                    % remove them from the triangulation
+I = find(sum(t1 < n,2) > 0);    % find triangles with no vertex in original domain
+t2 = t1(I,:);                   % remove them from the triangulation
+t3 = p_map(t2);                 % map node numbers
