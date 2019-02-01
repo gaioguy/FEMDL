dc56c5307d6355c66ec6284b795bf8b49cf84627
gaioguy <oj@tum.de>
2019-02-01 18:58:52 +0100

    fixed cylindrical Delaunay triangulation bug

--- src/delaunay_C2.m
+++ src/delaunay_C2.m
@@ -1,14 +1,21 @@
-function t3 = delaunay_C2_new(p, box)
+function t = delaunay_C2(p, dx)
 
-n = size(p,1);
-dx = box(2)-box(1); 
-dp = [dx 0; -dx 0];             
-P = [p; p+dp(1,:); p+dp(2,:)];  % copy vertices to the left and the right
-p_map = [1:n, 1:n, 1:n];
+%% DELAUNAY_C2 construct Delaunay triangulation on 2d cylinder
+%
+% t = delaunay_C2(p, dx) constructs a Delaunay triangulation on the 2d flat
+%   cylinder (i.e. periodic in x-direction)
+%   p: (n x 2), nodes of the triangulation, one node per row
+%   dx: size of the domain in x-direction
+%   t: (m3 x 3), triangulation, each row defines one triangle by indexing into p
+%
+% (C) 2019 by O. Junge, see COPYRIGHT 
 
-t = delaunay(P(:,1),P(:,2));    % construct triangulation
-I = find(sum(t > 2*n,2) == 0);  % find triangles with at least one vertex in left copy of domain
-t1 = t(I,:);                    % remove them from the triangulation
-I = find(sum(t1 < n,2) > 0);    % find triangles with no vertex in original domain
-t2 = t1(I,:);                   % remove them from the triangulation
-t3 = p_map(t2);                 % map node numbers
+n = size(p,1);            
+P = [p; p+[dx 0]; p-[dx 0]];    % copy vertices to the left and the right
+p_map = [1:n, 1:n, 1:n];        % map from extended nodes to original ones
+
+t0 = delaunay(P(:,1),P(:,2));   % construct triangulation
+% find triangles with no vertex in left copy and at least one in original domain
+I = find(sum(t0 > 2*n,2) == 0 & sum(t0 < n,2) > 0);  
+t = p_map(t0(I,:));             % remove rest from triangulation and map node numbers
+
