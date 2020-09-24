04a1005f7d1ee379b9d86ad356c7eb660984a57f
gaioguy <oj@tum.de>
2020-09-24 18:21:07 +0200

    bug fixed

--- src/2d/delaunay_T2.m
+++ src/2d/delaunay_T2.m
@@ -17,14 +17,14 @@ p1 = [p0;           p0+[dx 0];   p0+[0 dy]; ...
 pb1 = repmat(1:n,1,9);              % map from extended nodes to original ones
 
 Dt = delaunayTriangulation(p1);     % construct triangulation on extended nodes
-t1 = Dt.ConnectivityList;    
+t1 = Dt.ConnectivityList; 
 f1 = sum(t1 > 4*n,2) == 0;          % triangles with no vertex in left or lower copies
 f2 = sum(t1 <= n,2) > 0;            % triangles with at least one vertex in original domain
 I = find(f1 & f2);                  % triangles with both properties
 d = 1e-15;
-crns = [d d; dx-d d; d dy-d; dx-d dy-d];
-crnsI = pointLocation(Dt,crns);
-I = [I;crnsI];
+crn = [dx-d dy-d];
+crnI = pointLocation(Dt,crn);
+I = [I;crnI];
 t2 = t1(I,:);                       % remove all triangles not in I from triangulation
 J = unique(t2);                     % node numbers appearing in t1
 Jinv(J) = find(J);                  % inverse of J as a map on node numbers
@@ -34,7 +34,9 @@ mesh.pb = [1:length(J); pb1(J)]';   % and corresponding map for the boundary nod
 mesh.b = [];
 
 % scatter(p1(:,1),p1(:,2),10,'filled','b'); hold on
+% plot([0 dx dx 0 0], [0 0 dy dy 0],'k','linewidth',2);
 % triplot(t1,p1(:,1),p1(:,2),'b');
 % scatter(mesh.p(:,1),mesh.p(:,2),10,'filled','r'); 
-% triplot(mesh.t,mesh.p(:,1),mesh.p(:,2),'r'); hold off
+% scatter(p0(:,1),p0(:,2),30,'k','linewidth',2); 
+% triplot(mesh.t,mesh.p(:,1),mesh.p(:,2),'r','linewidth',2); hold off
 
