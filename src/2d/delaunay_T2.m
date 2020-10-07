f5cf550782786626508aaf6366eedefc9af6f402
gaioguy <oj@tum.de>
2020-10-07 17:57:23 +0200

    new version with y.a.b.f.

--- src/2d/delaunay_T2.m
+++ src/2d/delaunay_T2.m
@@ -8,35 +8,31 @@ function mesh = delaunay_T2(p0, dx, dy)
 %   dx, dy: size of the domain in x- and y-direction
 %   mesh: triangle mesh as produced by trimesh
 %
-% (C) 2019 by O. Junge, see COPYRIGHT 
+% (C) 2020 by O. Junge, see COPYRIGHT 
 
-n = size(p0,1);            
-p1 = [p0;           p0+[dx 0];   p0+[0 dy]; ...
-      p0+[dx dy];   p0+[-dx dy]; p0+[-dx 0]; ...
-      p0+[-dx -dy]; p0+[0 -dy];  p0+[dx -dy]];    % copy vertices
-pb1 = repmat(1:n,1,9);              % map from extended nodes to original ones
+n = size(p0,1);           
+p = [p0;           p0+[dx 0];   p0+[0 dy]; ...
+     p0+[dx dy];   p0+[-dx dy]; p0+[-dx 0]; ...
+     p0+[-dx -dy]; p0+[0 -dy];  p0+[dx -dy]];    % copy vertices
+pb = repmat(1:n,1,9);               % map from extended nodes to original ones
 
-Dt = delaunayTriangulation(p1);     % construct triangulation on extended nodes
-t1 = Dt.ConnectivityList; 
-f1 = sum(t1 > 4*n,2) == 0;          % triangles with no vertex in left or lower copies
-f2 = sum(t1 <= n,2) > 0;            % triangles with at least one vertex in original domain
-I = find(f1 & f2);                  % triangles with both properties
-d = 1e-15;
-crn = [dx-d dy-d];
-crnI = pointLocation(Dt,crn);
-I = unique([I;crnI]);
-t2 = t1(I,:);                       % remove all triangles not in I from triangulation
-J = unique(t2);                     % node numbers appearing in t2
+Dt = delaunayTriangulation(p);      % construct triangulation on extended nodes
+t = Dt.ConnectivityList;        
+t = t(find(sum(t <= n,2) > 0),:);   % triangles with at least one vertex in original domain
+[~,I] = unique(sort(pb(t),2),'rows'); % remove duplicate triangles
+t = t(I,:);                         % resulting reduced triangulation
+J = unique(t);                      % used nodes
 Jinv(J) = find(J);                  % inverse of J as a map on node numbers
-mesh.p = p1(J,:);                   % corresponding nodes
-mesh.t = Jinv(t2);
-mesh.pb = [1:length(J); pb1(J)]';   % and corresponding map for the boundary nodes
+
+mesh.p = p(J,:);                    % corresponding nodes
+mesh.t = Jinv(t);
+mesh.pb = [1:length(J); pb(J)]';    % and corresponding map for the boundary nodes
 mesh.b = [];
 
 % scatter(p1(:,1),p1(:,2),10,'filled','b'); hold on
 % plot([0 dx dx 0 0], [0 0 dy dy 0],'k','linewidth',2);
 % triplot(t1,p1(:,1),p1(:,2),'b');
-% scatter(mesh.p(:,1),mesh.p(:,2),10,'filled','r'); 
-% scatter(p0(:,1),p0(:,2),30,'k','linewidth',2); 
-% triplot(mesh.t,mesh.p(:,1),mesh.p(:,2),'r','linewidth',2); hold off
+% triplot(mesh.t,mesh.p(:,1),mesh.p(:,2),'r','linewidth',2); 
+% scatter(p0(:,1),p0(:,2),40,'g','linewidth',2); 
+% hold off; axis tight
 
